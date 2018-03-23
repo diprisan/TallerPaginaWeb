000bf2f7e4a782531267a4702cf497523b283906
galoarellano87 <35056722+galoarellano87@users.noreply.github.com>
2018-03-23 18:59:24 -0500

    Add files via upload
    Commit de imagenes con mapa

--- fuccion.js
+++ fuccion.js
@@ -1,24 +1,27 @@
- 
-      var map;
+ var map;
       var pyrmont = {lat: -2.191198, lng:-79.882160};
           
     function initialize() {
-         map = new google.maps.Map(document.getElementById('map'), {
+	    alert('Prueb');
+        map = new google.maps.Map(document.getElementById('map'), {
             center: pyrmont,
             zoom: 14
         });
         
-        infowindow = new google.maps.InfoWindow();
+		
+		 infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
           location: pyrmont,
-          radius: 1000,
-          type: ['store']
+          radius: 1500,
+          type: ['parking']
         }, callback);
+		
+        }
 
-        
+       
       
-    }
+
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
@@ -32,12 +35,10 @@
             map: map,
             position: place.geometry.location
         });
-
-        google.maps.event.addListener(marker, 'click',function(){
-            infowindow.setContent(place.name);
-            infowindow.open(map,this);
-            
-        var panorama = new google.maps.StreetViewPanorama(
+		google.maps.event.addListener(marker, 'click',function(){
+		infowindow.setContent(place.name);
+		infowindow.open(map,this);
+		var panorama = new google.maps.StreetViewPanorama(
             document.getElementById('pano'), {
                 position: place.geometry.location,
                 pov: {
@@ -46,14 +47,6 @@
                 }
             });
         map.setStreetView(panorama);
-            
-    
-            })
+		})
     }
-/*key=AIzaSyBg4Atm6V-CN0yBOFzBhPwXBwIs-V8pNQo
-function initMap() {
-    
-     map = new google.maps.Map(document.getElementById('map'), {
-       center: pyrmont,
-       zoom: 15,
-     });*/
+
